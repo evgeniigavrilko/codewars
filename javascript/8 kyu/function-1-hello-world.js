368ec9302b2a1db4a055644a95b3c067f3a63e11
evgeniigavrilko <155374565+evgeniigavrilko@users.noreply.github.com>
2024-01-08 22:41:28 +0500

    init: add solution "Function 1 - hello world" kata
    Add solution "Function 1 - hello world" kata.

---
+++ javascript/8 kyu/function-1-hello-world.js
@@ -0,0 +1,11 @@
+//8 kyu
+//Function 1 - hello world
+
+//Link:
+//https://www.codewars.com/kata/function-1-hello-world
+
+//Solution:
+function greet() {
+  const greetMessage = "hello world!";
+  return greetMessage;
+}
