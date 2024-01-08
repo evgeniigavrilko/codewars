64a0c0f37a6d169ac0f46288091726eb13206b71
evgeniigavrilko <155374565+evgeniigavrilko@users.noreply.github.com>
2024-01-08 22:54:48 +0500

    init: add solution "Opposite number" kata
    Add solution "Opposite number" kata.

---
+++ opposite-number.js
@@ -0,0 +1,11 @@
+//8 kyu
+//Opposite number
+
+//Link:
+//https://www.codewars.com/kata/opposite-number
+
+//Solution:
+function opposite(number) {
+  const result = 0 - number;
+  return (result);
+}
