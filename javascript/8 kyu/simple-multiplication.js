56dff4f56d4e6c2da811b8be48f9a0af19abf571
evgeniigavrilko <155374565+evgeniigavrilko@users.noreply.github.com>
2024-01-09 23:33:06 +0500

    init: add solution "Simple multiplication" kata
    Add solution "Simple multiplication kata.

---
+++ javascript/8 kyu/simple-multiplication.js
@@ -0,0 +1,8 @@
+//8 kyu
+//Simple multiplication
+
+//Link:
+//https://www.codewars.com/kata/simple-multiplication
+
+//Solution:
+const simpleMultiplication = (value) => (value % 2 === 0) ? value * 8 : value * 9;
