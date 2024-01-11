b2c4527737a7fcab525e2db08024e9b8194b11ae
evgeniigavrilko <155374565+evgeniigavrilko@users.noreply.github.com>
2024-01-11 23:51:26 +0500

    init: add solution "Remove First and Last Character" kata
    Add solution "Remove First and Last Character" kata.

---
+++ javascript/8 kyu/remove-first-and-last-character.js
@@ -0,0 +1,8 @@
+//8 kyu
+//Remove First and Last Character
+
+//Link:
+//https://www.codewars.com/kata/remove-first-and-last-character
+
+//Solution:
+const removeChar = (str) => (str.slice(1, -1));
