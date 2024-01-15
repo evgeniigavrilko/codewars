8d327b4e02b22f43a6b9cf8f0d7cccab178b1ffd
evgeniigavrilko <155374565+evgeniigavrilko@users.noreply.github.com>
2024-01-15 23:06:21 +0500

    init: add solution "Capitalization and Mutability" kata
    Add solution "Capitalization and Mutability" kata.

---
+++ javascript/8 kyu/capitalization-and-mutability.js
@@ -0,0 +1,8 @@
+//8 kyu
+//Capitalization and Mutability
+
+//Link:
+//https://www.codewars.com/kata/capitalization-and-mutability
+
+//Solution:
+const capitalizeWord = (word) => word.charAt(0).toUpperCase() + word.slice(1);
