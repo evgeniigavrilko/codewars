020899f657014d5ecef0f718e9214789fd21450f
evgeniigavrilko <155374565+evgeniigavrilko@users.noreply.github.com>
2024-01-21 17:16:50 +0500

    init: add solution "Largest Square Inside A Circle" kata
    Add solution "Largest Square Inside A Circle" kata.

---
+++ javascript/8 kyu/largest-square-inside-a-circle.js
@@ -0,0 +1,8 @@
+//8 kyu
+//Largest Square Inside A Circle
+
+//Link:
+//https://www.codewars.com/kata/largest-square-inside-a-circle
+
+//Solution:
+const areaLargestSquare = (r) => (Math.pow(r, 2) * 2);
