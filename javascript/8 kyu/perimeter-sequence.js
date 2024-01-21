61758795cc1f7d8466d84981b704e25d604abad4
evgeniigavrilko <155374565+evgeniigavrilko@users.noreply.github.com>
2024-01-21 16:54:48 +0500

    init: add solution "Perimeter sequence" kata
    Add solution "Perimeter sequence" kata.

---
+++ javascript/8 kyu/perimeter-sequence.js
@@ -0,0 +1,11 @@
+//8 kyu
+//Perimeter sequence
+
+//Link:
+//https://www.codewars.com/kata/perimeter-sequence
+
+//Solution:
+function perimeterSequence(a, n) {
+  const result = a * n * 4;
+  return result;
+}
