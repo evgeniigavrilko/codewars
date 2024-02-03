207a097f9b301db9f7eaed47a65b7dfb00de628a
evgeniigavrilko <155374565+evgeniigavrilko@users.noreply.github.com>
2024-02-03 21:07:08 +0500

    init: add solution "Get decimal part of the given number" kata
    Add solution "Get decimal part of the given number" kata.

---
+++ javascript/7 kyu/get-decimal-part-of-the-given-number.js
@@ -0,0 +1,10 @@
+//8 kyu
+//Get decimal part of the given number
+
+//Link:
+//https://www.codewars.com/kata/get-decimal-part-of-the-given-number
+
+//Solution:
+function getDecimal(n) {
+  return (Math.abs(n % 1));
+}
